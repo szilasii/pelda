464ed6a59a55bb09b15602e0f2ffaa0ab61816aa
szilasii <szilasii78@gmail.com>
2023-12-05 09:45:49 +0100

    token end

--- auth/auth.js
+++ auth/auth.js
@@ -2,7 +2,19 @@ const jwt = require("jsonwebtoken")
 const config = require("../App/config")
 
 function verifyToken(req,res,next) {
-    
+    const token = req.body?.token || req.query?.token || req.headers?.['x-access-token'];
+    if (!token) {
+          return res.status(403).send("Token szükséges a hozzáféréshez!")  
+    }
+
+    try {
+        const decodedToken = jwt.verify(token,process.env.JWT_STRING);
+        console.log(decodedToken);
+        req.user = decodedToken;
+    } catch(e) {
+        return res.status(401).send("Hibás token");        
+    }
+    return next();
 }
 
 module.exports = verifyToken
--- user/router.js
+++ user/router.js
@@ -1,7 +1,8 @@
 var user = require('./user-model');
 var router = require('express').Router();
+const auth = require("../auth/auth");
 
-router.get('/user',user.getAllUserInfos);
+router.get('/user',auth,user.getAllUserInfos);
 router.get('/user/:id',user.getUserDataFromId);
 router.post('/user/reg',user.regUser);
 router.post('/user/:id/address',user.createNewAddress);
